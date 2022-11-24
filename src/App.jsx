46a0c11c2cb9e5a1eb3cc4cbf1125bb23a0836b9
José <jg345762@gmail.com>
2022-11-24 14:20:10 -0300

    Birthday complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,23 @@ import Person from "./components/Person";
 function App() {
   const { birthdays, setBirthdays } = useGlobalContext();
   return (
-    <div>
-      <section>
-        <div>
-          <h1>{birthdays.length} birthdays today</h1>
+    <div className="min-h-screen bg-black">
+      <section className="px-5 sm:px-8 flex justify-center items-center min-h-screen">
+        <div className="container sm:max-w-md bg-white p-5 rounded">
+          <h1 className="text-2xl sm:text-4xl font-bold mb-5">
+            {birthdays.length} birthdays today
+          </h1>
           <div>
             {birthdays.map((item) => {
               return <Person key={item.id} item={item} />;
             })}
           </div>
-          <button onClick={() => setBirthdays([])}>Clear All</button>
+          <button
+            onClick={() => setBirthdays([])}
+            className="w-full bg-black text-white rounded p-2 text-xl sm:text-2xl"
+          >
+            Clear All
+          </button>
         </div>
       </section>
     </div>
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -2,13 +2,17 @@ import React from "react";
 
 function Person({ item }) {
   return (
-    <div>
-      <div>
-        <img src={item.image} alt={item.name} />
+    <div className="flex mb-5">
+      <div className="w-16 h-16 mr-2">
+        <img
+          src={item.image}
+          alt={item.name}
+          className="w-full h-full rounded-full"
+        />
       </div>
-      <div>
-        <h2>{item.name}</h2>
-        <p>{item.age}</p>
+      <div className="text-lg sm-text-xl">
+        <h2 className="font-semibold">{item.name}</h2>
+        <p className="text-gray-500">{item.age} years</p>
       </div>
     </div>
   );
